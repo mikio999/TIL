64bc79ef3a84bb94489a2508d5bd17c45c77f3ab
minkyung_kwak <ellen10min@gmail.com>
2023-02-04 23:59:14 +0900

    Add: Algorithm  조건문 문제 풀이

---
+++ Algorithm/Baekjoon/조건문/1330_두 수 비교하기/1330.js
@@ -0,0 +1,11 @@
+let input = require("fs").readFileSync("/dev/stdin").toString().split(" ");
+const A = Number(input[0]);
+const B = Number(input[1]);
+
+if (A > B) {
+  console.log(">");
+} else if (A < B) {
+  console.log("<");
+} else {
+  console.log("=");
+}
--- Algorithm/Intro.md
+++ Algorithm/Intro.md
@@ -1,1 +1,13 @@
 > 알고리즘 문제 정리
+> 활용 사이트 : 백준, LeetCode
+> 사용 언어 : python, javascript
+
+## 백준
+
+---
+
+- 단계별 문제
+
+## LeetCode
+
+---
